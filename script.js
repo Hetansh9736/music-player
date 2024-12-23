17ce9f61f5bbd0fe508060835984da0706c4151c
Hetansh <hetanshsachaniya07@gmail.com>
2024-12-23 23:59:09 +0530

    Basic-Done

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 
 function plays() {
-
     let audio = document.querySelector(".audio")
-
-
-
     if (audio.paused) {
         audio.play();
         console.log("play");
@@ -23,38 +19,41 @@ let resume = document.querySelector(".pause")
 let list = [
     { song: "Victory Anthem", address: "music/Music-01.m4a", img: "Images/victory.jpg" },
     { song: "Millioner", address: "music/milioner.mp3", img: "Images/milioner.jpg" },
-    { song: "Childhood", address: "music/childhood.mp3", img: "Images/victory.jpg" },
-    { song: "SkyFall", address: "music/skyfall.mp3", img: "Images/victory.jpg" },
-    { song: "Gangster", address: "music/gangster.mp3", img: "Images/GP.jpg" }
+    { song: "Childhood", address: "music/childhood.mp3", img: "Images/childhood.jpeg" },
+    { song: "SkyFall", address: "music/skyfall.mp3", img: "Images/skyfall.jpg" },
+    { song: "Gangster", address: "music/gangster.mp3", img: "Images/GP.jpeg" }
 ]
 
 let i = 0;
 
 function next() {
     let audio = document.querySelector(".audio")
-    let poster= document.getElementById("poster")
+    let poster = document.getElementById("poster")
     let name = document.getElementById("s-name")
     audio.pause()
-   document.getElementById("pp-img").src = "Images/Resume.png"
+    document.getElementById("like").src = "Images/like.png";
+    document.getElementById("pp-img").src = "Images/Resume.png"
 
     if (i == list.length - 1) {
         i = 0
     } else {
         i++
     }
-    
 
     audio.src = list[i].address
     audio.play();
-
+    document.getElementById("pp-img").src = "Images/pause.png"
+    document.getElementById("download").href = list[i].address
+    poster.src = list[i].img
+    name.innerHTML = list[i].song
 
 }
 function pre() {
-    
-
     let audio = document.querySelector(".audio")
-    let poster= document.getElementById("poster")
+    let poster = document.getElementById("poster")
     let name = document.getElementById("s-name")
+    document.getElementById("like").src = "Images/like.png";
+    
     console.log
     audio.pause()
     document.getElementById("pp-img").src = "Images/Resume.png"
@@ -65,33 +64,44 @@ function pre() {
         i--
     }
 
-    poster.src= list[i].img
-    name.innerHTML= list[i].song
-
-   
+    poster.src = list[i].img
+    name.innerHTML = list[i].song
 
     audio.src = list[i].address
     audio.play();
     document.getElementById("pp-img").src = "Images/pause.png"
+    document.getElementById("download").href = list[i].address
 }
 
 
-// document.addEventListener("DOMContentLoaded", function () {
-//     let audio = document.querySelector(".audio")
-//     let progress = document.querySelector("#progress")
+document.addEventListener("DOMContentLoaded", function () {
+    let audio = document.querySelector(".audio")
+    let progress = document.querySelector("#progress")
 
-//     audio.onloadedmetadata = function () {
-//         progress.max = audio.duration
-//         progress.value = audio.currentTime
-//     }
+    audio.onloadedmetadata = function () {
+        progress.max = audio.duration
+        progress.value = audio.currentTime
+    }
 
-//     if (audio.play()) {
-//         setInterval(() => {
-//             progress.value = audio.currentTime
-//         }, 500);
-//     }
+    if (audio.play()) {
+        setInterval(() => {
+            progress.value = audio.currentTime
+        }, 500);
+    }
 
-//     progress.onchange = function () {
-//         audio.currentTime = progress.value
-//     }
-// })
+    progress.onchange = function () {
+        audio.currentTime = progress.value
+    }
+})
+
+let j = 0;
+function liked() {
+    let like = document.getElementById("like");
+    if (j % 2 == 0) {
+        like.src = "Images/liked.png"
+    } else {
+        like.src = "Images/like.png"
+    }
+    j++
+}
+ 
