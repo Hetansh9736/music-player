e8b25a2b46bd59898fa86f20a83fda3c8b5125de
bhumit <bhumitsachaniya1010@gmail.com>
2024-12-23 16:47:10 +0530

    finding logic

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 let audio = document.querySelector(".audio")
 let previous = document.querySelector(".pre")
-let pause = document.querySelector(".pause")
+let resume = document.querySelector(".pause")
 let next = document.querySelector(".next")
+
+function plays(){
+    if (audio.paused) {
+        audio.play(); // Play the song if it is paused
+        console.log("play");
+        // pause.innerHTML = '<img src="Images/play.png" alt="Play">'; // Update icon to play
+    } else {
+        audio.pause(); // Pause the song if it is playing
+        console.log("pause");
+    
+        // pause.innerHTML = '<img src="Images/pause.png" alt="Pause">'; // Update icon to pause
+    }
+}
+
+
+
+
+
+
 
