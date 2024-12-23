94431ec9167f2ae71638cdb2ce815eaf7edf8284
bhumit <bhumitsachaniya1010@gmail.com>
2024-12-23 18:59:34 +0530

    pre and next done

--- script.js
+++ script.js
@@ -1,33 +1,72 @@
 
 function plays(){
-
-let audio = document.querySelector(".audio")
-
-
+    
+    let audio = document.querySelector(".audio")
+    
+    
 
     if (audio.paused) {
         audio.play();
         console.log("play");
-        console.log(document.getElementsByClassName("pp-img")[0].src="Images/pause.png")
+        console.log(document.getElementById("pp-img").src="Images/pause.png")
     } else {
         audio.pause();
         console.log("pause");
-        console.log(document.getElementsByClassName("pp-img")[0].src="Images/Resume.png")
+        console.log(document.getElementById("pp-img").src="Images/Resume.png")
     }
 }
 
 let previous = document.querySelector(".pre")
 let resume = document.querySelector(".pause")
-let next = document.querySelector(".next")
 
-let music_list = [
+
+let list = [
     {song1:"victory Anthem",address:"music/Music-01.m4a" ,img:"Images/victory.jpg"},
-    {song1:"victory Anthem",address:"music/Music-01.m4a" ,img:"Images/victory.jpg"},
-    {song1:"victory Anthem",address:"music/Music-01.m4a" ,img:"Images/victory.jpg"},
-    {song1:"victory Anthem",address:"music/Music-01.m4a" ,img:"Images/victory.jpg"},
+    {song1:"millioner",address:"music/milioner.mp3" ,img:"Images/milioner.jpg"},
+    {song1:"victory Anthem",address:"music/childhood.mp3" ,img:"Images/victory.jpg"},
+    {song1:"victory Anthem",address:"music/skyfall.mp3" ,img:"Images/victory.jpg"},
     ]
 
+    let i= 0;
+
+function next(){
+    let next = document.querySelector(".next")
+   
+    let audio = document.querySelector(".audio")
+    audio.pause()
+    console.log(document.getElementById("pp-img").src="Images/Resume.png")
+    
+    if(i==list.length-1){
+        i=0
+    }else{
+        i++
+    }
+    console.log(audio.src = list[i].address)
+    
+    audio.src = list[i].address
+    
+    
+    
+}
+function pre(){
+    let next = document.querySelector(".next")
+   
+    let audio = document.querySelector(".audio")
+    audio.pause()
+    console.log(document.getElementById("pp-img").src="Images/Resume.png")
+    
+    if(i==0){
+        i=list.length
+    }else{
+        i--
+    }
+    
+    console.log(audio.src = list[i].address)
+    
+    audio.src = list[i].address
 
 
+
+}
 
 
