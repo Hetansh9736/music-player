195c47a23e18e2441dc5a69efb34cc980dc376a0
Hetansh <hetanshsachaniya07@gmail.com>
2024-12-23 21:22:56 +0530

    Data formatted

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 
-function plays(){
-    
+function plays() {
+
     let audio = document.querySelector(".audio")
-    
-    
+
+
 
     if (audio.paused) {
         audio.play();
         console.log("play");
-        console.log(document.getElementById("pp-img").src="Images/pause.png")
+        console.log(document.getElementById("pp-img").src = "Images/pause.png")
     } else {
         audio.pause();
         console.log("pause");
-        console.log(document.getElementById("pp-img").src="Images/Resume.png")
+        console.log(document.getElementById("pp-img").src = "Images/Resume.png")
     }
 }
 
@@ -21,48 +21,48 @@ let resume = document.querySelector(".pause")
 
 
 let list = [
-    {song1:"victory Anthem",address:"music/Music-01.m4a" ,img:"Images/victory.jpg"},
-    {song1:"millioner",address:"music/milioner.mp3" ,img:"Images/milioner.jpg"},
-    {song1:"victory Anthem",address:"music/childhood.mp3" ,img:"Images/victory.jpg"},
-    {song1:"victory Anthem",address:"music/skyfall.mp3" ,img:"Images/victory.jpg"},
-    ]
+    { song1: "victory Anthem", address: "music/Music-01.m4a", img: "Images/victory.jpg" },
+    { song1: "millioner", address: "music/milioner.mp3", img: "Images/milioner.jpg" },
+    { song1: "victory Anthem", address: "music/childhood.mp3", img: "Images/victory.jpg" },
+    { song1: "victory Anthem", address: "music/skyfall.mp3", img: "Images/victory.jpg" },
+]
 
-    let i= 0;
+let i = 0;
 
-function next(){
+function next() {
     let next = document.querySelector(".next")
-   
+
     let audio = document.querySelector(".audio")
     audio.pause()
-    console.log(document.getElementById("pp-img").src="Images/Resume.png")
-    
-    if(i==list.length-1){
-        i=0
-    }else{
+    console.log(document.getElementById("pp-img").src = "Images/Resume.png")
+
+    if (i == list.length - 1) {
+        i = 0
+    } else {
         i++
     }
     console.log(audio.src = list[i].address)
-    
+
     audio.src = list[i].address
-    
-    
-    
+
+
+
 }
-function pre(){
+function pre() {
     let next = document.querySelector(".next")
-   
+
     let audio = document.querySelector(".audio")
     audio.pause()
-    console.log(document.getElementById("pp-img").src="Images/Resume.png")
-    
-    if(i==0){
-        i=list.length
-    }else{
+    console.log(document.getElementById("pp-img").src = "Images/Resume.png")
+
+    if (i == 0) {
+        i = list.length
+    } else {
         i--
     }
-    
+
     console.log(audio.src = list[i].address)
-    
+
     audio.src = list[i].address
 
 
