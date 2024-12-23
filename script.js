c50b2151d834b48ced3c9e0d02430f1938ed2c4d
Hetansh <hetanshsachaniya07@gmail.com>
2024-12-23 16:26:04 +0530

    Js

---
+++ script.js
@@ -0,0 +1,5 @@
+let audio = document.querySelector(".audio")
+let previous = document.querySelector(".pre")
+let pause = document.querySelector(".pause")
+let next = document.querySelector(".next")
+
